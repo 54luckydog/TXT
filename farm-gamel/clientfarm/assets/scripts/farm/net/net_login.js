90ca79f8c012278f2f628a69364461c16aa21d1c
zylluckdog <3545163168@qq.com>
2019-09-06 17:57:45 +0800

    V

--- farm-gamel/clientfarm/assets/scripts/farm/net/net_login.js
+++ farm-gamel/clientfarm/assets/scripts/farm/net/net_login.js
@@ -6,28 +6,65 @@
 
 const TAG = 'NET_LOGIN';
 const Log = GS.Log.create(TAG);
-class Login{
-    constructor(){
-        this.globalData={
-            "userInfo":""
-        }
-        /*
-        this._code="";
-        this._appId="";
-        this._secret="";
-        this._grandType="authorization_code";
-        this._openId="";//用户唯一id
-        this._session_key="";//会话密钥
-
-        this._nickName="";
-        this._avatarUrl=""//头像
-        */
-    };
+const UUID=GS.Utils;
+class Login {
     
-    onLoad () {
+    constructor() {
+        this._code = "";
+        this._nickName = "";
+        this._imageUrl = "";
+        this._encryptedData = "";
+        this._iv = "";
+        
+    
+    };
+    onLoad(){
+        this.checkUserInfoPermission(); //检查用户限权设置
+    };
+    doLogin() {
+        Log.debug("登陆")
+        wx.login({
+            "timeout": 1500,
+            success: function (loginRes) {
+                if (loginRes.code) {
+                    this._code = loginRes.code;
+                    //获取code成功 
+                    wx.getUserInfo({
+                        withCreadentials: true, //携带登录态信息
+                        success: function (infoRes) {
+                            this.userInfo = infoRes.userInfo;
+                            this._nickName = userInfo.nickName;
+                            this._imageUrl = userInfo.imageUrl;
+                            this._encryptedData = res.encryptedData;
+                            this._iv = res.iv;
+                            this.request;
+                        },
+
+                        fail: function () {
+                            //获取用户信息失败 检查是否打开限权
+                            wx.hideLoading();
+                            
+                        }
+                    });
+                } else {
+                    this.showInfo("登陆失败");
+                    Log.debug("调用wx.login获取code失败")
+                }
+            },
+            fail: function (error) {
+
+                this.showInfo("接口调用失败")
+                Log.debug(error)
+
+            },
+        })
+
+    }
+    /*
+    onLoad() {
         this.checkLoginStatus();
     }
-
+    
     checkLoginStatus() {
         Log.debug("检查是否有登陆标识")
         let loginFlag = wx.getStorageSync("loginFlag");
@@ -35,7 +72,7 @@ class Login{
             //检查是否过期
             wx.checkSession({
                 success: function () {
-                    let userStorageInfo = wx.getStorageSync("userInfo"); //从本地缓存中同步获取key对应的value值
+                    let userStorageInfo = wx.getStorage("userInfo"); //从本地缓存中异步获取key对应的value值
                     if (userStorageInfo) {
                         this.globalData.userInfo = JSON.parse(userStorageInfo); //把获取到的值传给全局变量中的userinfo
                     } else {
@@ -44,108 +81,17 @@ class Login{
                     }
                 },
                 fail: function () {
-                    this.doLogin();
+                    this.checkUserInfoPermission();
                 }
             });
         } else {
             Log.debug("没有登录态")
-            this.doLogin();
+            this.checkUserInfoPermission();
         }
     }
+    */
 
-    doLogin() {
-        Log.debug("登陆")
-        this._uuid=uuid.creatUUID()
-        wx.login({ 
-            "timeout" : 1500,
-            success: function (loginRes) {
-                this._code=loginRes.code
 
-                if (loginRes.code) {
-                    //获取code成功 
-                    wx.getUserInfo({
-                        withCreadentials: true,
-                        success: function (infoRes) {
-                            this.userInfo = infoRes.userInfo
-                            this.nickName = userInfo.nickName 
-                            this.imageUrl = userInfo.imageUrl 
-                            this.unionId = userInfo.unionId
-                            wx.request({
-                                url: "",//向服务器请求登陆
-                                method:POST,
-                                data: {
-                                    "code": loginRes.code, //临时登陆凭证                                      
-                                },
-                                sucess: function (res) {
-                                    Log.debug("登陆成功")
-                                    Data = res.data;
-
-                                    if (res.code == 0) {
-
-                                        this.globalData.userInfo = Data.userInfo;
-
-                                        wx.setStorageSync('userInfo', JSON.stringify(res.userInfo)); //将数据存储在本地缓存中指定的 key 中。
-                                        wx.setStorageSync('loginFlag', JSON.stringify(res.loginFlag));
-
-                                        wx.request({
-                                            url:'',
-                                            method:POST,
-                                            data:{
-                                                uuid:this.uuid,
-                                                unionId:this.unionId,
-                                                nickName:this.nickName,
-                                                imageUrl:this.imageUrl,
-                                            },
-                                            
-                                            success:function(res){
-
-                                                if(res.code==0){
-                                                   
-                                                   this._loginData=res.data
-                                                  // return(this.loginData)
-                                                }else{
-                                                    this.showInfo(res.msg)
-                                                }
-
-                                            },
-                                            fail:function(res){
-                                                this.showInfo(res.msg)
-                                            }
-
-                                        })  
-                                    } else {
-                                        this.showInfo(res.msg) //errmsg是错误信息 string类型
-                                    }
-                                },
-
-                                fail: function (res) {
-                                    //调用服务端登录接口失败
-                                    this.showInfo(res.msg)
-                                    console.log(msg);
-                                },
-                            });
-                        },
-                        fail: function () {
-                            //获取用户信息失败 检查是否打开限权
-                            wx.hideLoading();
-                            this.checkUserInfoPermission(); //检查用户限权设置
-                        }
-                    });
-                } else {
-                    this.showInfo("登陆失败");
-                    Log.debug("调用wx.login获取code失败")
-                }
-
-            },
-            fail: function (error) {
-               
-                this.showInfo("接口调用失败")
-                Log.debug(error)
-                
-            },
-        })
-
-    }
     //检查用户限权设置
     checkUserInfoPermission() {
         wx.getSetting({
@@ -153,10 +99,13 @@ class Login{
                 if (!res.authSetting['scope.userInfo']) {
                     wx.authorize({
                         scop: 'scope.userInfo',
-                        success() {},
+                        success() {
+                            this.doLogin()
+                        },
                         fail() {
                             wx.openSetting({
                                 success: function (res) {
+                                    this.doLogin()
                                     Log.debug("openSetting success")
                                 },
                                 fail: function (res) {
@@ -165,6 +114,8 @@ class Login{
                             });
                         }
                     });
+                } else {
+                    this.doLogin()
                 }
             },
             fail: function (error) {
@@ -173,18 +124,43 @@ class Login{
         });
 
     };
-    showInfo(title, icon ) {
+
+
+    request(){
+        this.uuid = UUID.Utils.creatUUID();
+        let reqData = {
+            code: this._code,
+            uuid: this.uuid,
+            encryptedData:this._encryptedData,
+            iv:this._iv,
+          //  unionId: this._unionId,
+            nickName: this._nickName,
+            imageUrl: this._imageUrl,
+        }
+        
+        GS.Http.httpPost("http://47.111.22.119:80/greenhouse/game/purchaseTools", reqData, function (responseJson) {
+            if (responseJson.code === 0) {
+                //return responseJson.data
+                this._openId = responseJson.openid; 
+                this._session_key = responseJson.session_key 
+                wx.setStorageSync({
+                    'userInfo': JSON.stringify(responseJson.userInfo),
+                    'loginFlag': JSON.stringify(responseJson.loginFlag)
+                }); 
+                this.getData=responseJson.data
+            } else {}
+        })
+    }
+    showInfo(title, icon) {
         wx.showToast({
             title: title,
             icon: icon,
             duration: 1500,
-            mask: true 
+            mask: true
         });
 
     };
-    showloginData(){
-        return(this._loginData)
-    }
+   
 }
-let login=new Login();
-module.exports.Login=login
+let login = new Login();
+module.exports.Login = login
